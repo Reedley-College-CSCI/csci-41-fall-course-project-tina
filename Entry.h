6ec3a8f62fc4788cc69cc0ae5372d2bd8aa0d342
tinak1234 <0875616@my.scccd.edu>
2024-09-23 09:36:29 -0700

    Update Entry.h

--- Entry.h
+++ Entry.h
@@ -1,17 +1,34 @@
 #ifndef ENTRY_H
 #define ENTRY_H
 #include <string>
-
+#include <iostream>
 using namespace std;
+
 class Entry{
 private: 
-    string name;
-    int age;
+    string address;
+    double price;
+    int bedrooms;
+    int bathrooms;
+    string description;
 public:
     Entry();
-    Entry(string name0, int age0);
-    //write getters and setters and print
-}
+    Entry(string address, double price, int bedrooms, int bathrooms, string description);
+    //updated getters
+    string getAddress() const;
+    double getPrice() const;
+    int getBedrooms() const;
+    int getBathrooms() const;
+    string getDescription() const;
+//updated setters
+    void setAddress(const string& address);
+    void setPrice(double price);
+    void setBedrooms(int bedrooms);
+    void setBathrooms(int bathrooms);
+    void setDescription(const string& description);
+    //override stream operators
+    void print() const;
+};
 
 
 
