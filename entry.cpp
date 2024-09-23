903448b963202abac194db02974f924f0ef66003
tinak1234 <0875616@my.scccd.edu>
2024-09-23 09:38:07 -0700

    Update entry.cpp

--- entry.cpp
+++ entry.cpp
@@ -1,10 +1,15 @@
 #include "Entry.h"
 
+//default constructor
 Entry::Entry(){
     name = "";
     age = 0;
 }
-Entry::Entry(string name0, int age0) {
-    name = name0;
-    age = age0;
+//updated constructor with parameters
+Entry::Entry(string address, double price, int bedrooms, string descriptionc) {
+
+}
+//print the entry
+void Entry::print() {
+    std::cout << name << " " << age << std::endl;
 }
